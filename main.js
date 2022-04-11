3b11c3d6d38e41d2ba5a6cc683beb39c71f8c5ee
arjunycoding <arjunycoding@gmail.com>
2022-04-10 21:32:05 -0400

    hopfully it works ?!

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ function everything(keyPressed, keyCode, event = null) {
                 $(`#tile${i}`).addClass("right")
                 $(`#tile${i}`).addClass("speacialFlip")
                 console.log($(`#tile${i}`).val())
-                $(`.letter:contains(${($(`#tile${i}`).val()).toUpperCase()})`).addClass("right")
+                $(`.letter:contains(${($(`#tile${i}`).val())})`).addClass("right")
                 audioFiles.push(`letters/${($(`#tile${i}`).val()).toUpperCase()}`)
             }
             audioFiles.reverse()
